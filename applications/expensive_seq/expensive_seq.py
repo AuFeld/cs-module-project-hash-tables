8684857c645e4431e6959038c1497bedd8766ea2
Chase Goldfeld <goldfeld.chase@gmail.com>
2020-07-09 11:50:03 -0400

    day 3

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,24 @@
-# Your code here
+'''
+In Python, a dict key can be any immutable type, including a tuple
+'''
 
+# TO DO
+# 1. create cache
+# 2. create hashtable
+
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    # est kvp
+    key = (x, y, z)
+    # handle base case
+    if x <= 0:
+        return y + z 
+    # recurse if key is not already present
+    else:
+        if key not in cache:
+            cache[key] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return cache[key]
 
 
 
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,8 +1,29 @@
+import string
+
 def word_count(s):
-    # Your code here
+    # punc = 0
+    # ignore = list('":;,.-+=/\\|[]{}()*^&')
+    # If the input contains no ignored characters, return an empty dictionary.
+    # for i in ignore:
+    #   if i in s:
+    #       punc += 1
+    # if punc == 0:
+    #   return {}
 
+    # dict cache for word counts
+    counts = {}
+    # remove unwanted punctuation and convert to a list
+    s = s.lower().translate(str.maketrans("", "", '":;,.-+=/\\|[]{}()*^&'))
+    list_s = s.split()
+    # either increase the count if the word is found or add the word to cache
+    for word in list_s:
+        if word in counts: 
+            counts[word] += 1
+        else: 
+            counts[word] = 1
 
-
+    return counts 
+    
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
